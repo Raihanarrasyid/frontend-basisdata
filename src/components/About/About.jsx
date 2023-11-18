f73a503ac79891673afdd482fb09fae423a77569
Raihanarrasyid <raihanarrasyid1704@gmail.com>
2023-11-19 00:31:10 +0700

    progress

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,11 @@
-import React from "react";
-
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 export default function About() {
-  return <div>About</div>;
+  return (
+    <Box className="my-5">
+      <Typography variant="h2" className="text-white text-center my-5">
+        Tentang Manganly
+      </Typography>
+    </Box>
+  );
 }
